deb0fe240c7152183a2dfc087fef21ff684dd03e
CihatKOCAK <pcihatkocakks@gmail.com>
2024-12-11 17:39:55 +0300

    add initial files for frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
