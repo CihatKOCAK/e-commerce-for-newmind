dd45069eb4bf9d4d4eb3843bd12f1f40c0d28560
CihatKOCAK <pcihatkocakks@gmail.com>
2024-12-19 09:56:27 +0300

    Refactor Product model to include view count and rating information

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -6,7 +6,16 @@ const productSchema = new mongoose.Schema({
   price: { type: Number, required: true },
   stock: { type: Number, required: true },
   category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  viewCount: { type: Number, default: 0 }, // Ürün görüntülenme sayısı
+  rate: { type: Number, default: 0 }, // Ürün değerlendirme puanı
+  rateCount: { type: Number, default: 0 },
   image: { type: String }, // Ürün görsel URL'si
 }, { timestamps: true });
+
+// her get işlemi yapıldığında viewCount'u 1 arttır
+productSchema.methods.incrementViewCount = async function () {
+  this.viewCount += 1;
+  await this.save();
+};
 
 module.exports = mongoose.model("Product", productSchema);
--- frontend/src/components/Product/ProductCard.js
+++ frontend/src/components/Product/ProductCard.js
@@ -1,8 +1,9 @@
 import './ProductCard.css';
-import brokenImg from '../../assets/brokenImg.png'; // Dummy resim
+import brokenImg from '../../assets/brokenImg.png';
+import { FaStar } from 'react-icons/fa';
 
 const ProductCard = ({ product }) => {
-  const { name, description, price, stock, category, image } = product;
+  const { name, description, price, stock, category, image, rate, viewCount, rateCount } = product;
 
   return (<div className="product-card">
       <div className="product-image">
@@ -12,6 +13,15 @@ const ProductCard = ({ product }) => {
         <h3 className="product-name">{name}</h3>
         <p className="product-category">{category?.name || 'Uncategorized'}</p>
         <p className="product-description">{description}</p>
+        <span className="product-rating">
+          {[...Array(5)].map((_, index) => {
+            const ratingValue = index + 1;
+            return <FaStar key={index} color={ratingValue <= rate ? '#f8d64e' : '#e4e5e9'} />;
+          }
+          )}
+          <span className="product-rating-count">({rateCount ? rateCount : 0})</span>
+          <span className="product-view-count">({viewCount ? viewCount : 0}) views</span>
+        </span>
         <p className="product-price">${price.toFixed(2)}</p>
         <p className={`product-stock ${stock > 0 ? 'in-stock' : 'out-of-stock'}`}>
           {stock > 0 ? `${stock} in stock` : 'Out of stock'}
